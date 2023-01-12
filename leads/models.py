fdad330212de0f35d4d78949e8ffb8c0736a0cab
unknown <tara.zver22@gmail.com>
2023-01-12 08:42:28 +0100

    Model manager.

--- leads/models.py
+++ leads/models.py
@@ -11,6 +11,10 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #1 user to one agent -> one to one field
 
+    def __str__(self):
+        return self.user.email
+
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -22,4 +26,7 @@ class Lead(models.Model):
     # - models.CASCADE → delete a lead
     # - models.SETNULL +null=True → set it to NULL
     # - models.SET_DEFAULT + default=… → set it to a default
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
