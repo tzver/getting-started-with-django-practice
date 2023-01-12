f31cd21633d61530ef08cebe4bd25f2fad66577c
unknown <tara.zver22@gmail.com>
2023-01-12 11:01:40 +0100

    Update, delete

--- leads/forms.py
+++ leads/forms.py
@@ -1,4 +1,17 @@
 from django import forms
+from .models import Lead
+
+class LeadModelForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            "first_name",
+            "last_name",
+            "age",
+            "agent"
+        )
+            
+
 
 class LeadForm(forms.Form): #inherit from forms.Form
     # give fields of a form
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 def lead_list(request):
@@ -22,24 +22,83 @@ def lead_detail(request, pk):
     return render(request,"leads/lead_detail.html", context)
 
 
+
+
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            # the form doesn't include an agent -> have to add it yourself!
-            agent = Agent.objects.first() # -> first row in the table
-            Lead.objects.create( #creates a new lead in the DB
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
+            form.save()
             return redirect("/leads")
     context = {
         "form": form #create an instance of the form; can validate the info
     }
     return render(request, "leads/lead_create.html", context)
+
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead) #grab an instance and update it!
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request,"leads/lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("leads/")
+
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save() #commit the data to a DB
+#             return redirect("/leads")
+#     context = {
+#         "lead": lead,
+#         "form": form
+#     }
+#     return render(request,"leads/lead_update.html", context)
+
+
+
+
+# def lead_create(request):
+#     form = LeadForm()
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             # the form doesn't include an agent -> have to add it yourself!
+#             agent = Agent.objects.first() # -> first row in the table
+#             Lead.objects.create( #creates a new lead in the DB
+#                 first_name=first_name,
+#                 last_name=last_name,
+#                 age=age,
+#                 agent=agent
+#             )
+#             return redirect("/leads")
+#     context = {
+#         "form": form #create an instance of the form; can validate the info
+#     }
+#     return render(request, "leads/lead_create.html", context)
