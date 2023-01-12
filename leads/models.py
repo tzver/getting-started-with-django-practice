7e9154590eb13b9c2b59bb930865d086ef81a1ba
unknown <tara.zver22@gmail.com>
2023-01-12 07:48:47 +0100

    Fields

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Lead(models.Model):
+
+    SOURCE_CHOICES = (('YT', 'YouTube'), ('Google', 'Google'), ('Newsletter', 'Newsletter'))
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
