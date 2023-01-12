a28649202a4f94d18ca5571b89ba3d9be5344c91
unknown <tara.zver22@gmail.com>
2023-01-12 08:09:31 +0100

    CustomUser

--- leads/models.py
+++ leads/models.py
@@ -1,16 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Lead(models.Model):
+class User(AbstractUser):
+    # VERY IMPORTANT CHANGE WE SHOULD DO! -> to be able to make/add/make changes later in the process 
+    # cellphone_number = models.CharField(max_length=15)
+    pass
 
-    SOURCE_CHOICES = (('YT', 'YouTube'), ('Google', 'Google'), ('Newsletter', 'Newsletter'))
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #1 user to one agent -> one to one field
+
+class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    # Foreign key field -> relationships between models
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE) #if "Agent" -> means Agent is inside the file (otherwise order matters)
+    # on_delete => if agent is deleted, do: 
+    # - models.CASCADE → delete a lead
+    # - models.SETNULL +null=True → set it to NULL
+    # - models.SET_DEFAULT + default=… → set it to a default
 
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
