70ffc8e6059323cc4058b353f09a72584a510b94
unknown <tara.zver22@gmail.com>
2023-01-12 10:40:04 +0100

    Forms#2

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 # Create your views here.
 def lead_list(request):
@@ -22,4 +23,23 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    return render(request, "leads.lead_create.html")
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            # the form doesn't include an agent -> have to add it yourself!
+            agent = Agent.objects.first() # -> first row in the table
+            Lead.objects.create( #creates a new lead in the DB
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            return redirect("/leads")
+    context = {
+        "form": form #create an instance of the form; can validate the info
+    }
+    return render(request, "leads/lead_create.html", context)
