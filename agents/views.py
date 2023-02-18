ad77382f990bb7fb5d3601af08492921f73560a7
Tara <tara.zver22@gmail.com>
2023-02-18 13:26:20 +0100

    Filter Agents

--- agents/views.py
+++ agents/views.py
@@ -10,7 +10,8 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
@@ -32,7 +33,8 @@ class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     context_object_name = "agent"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
@@ -53,5 +55,6 @@ class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
         return reverse("agents:agent-list")
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
 
